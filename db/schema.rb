f7485ade76bbf05a7fac707e0f15ecb0e1f75f86
102306071 <10206071@nccu.edu.tw>
2015-05-01 13:03:53 +0800

    add username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150501041209) do
+ActiveRecord::Schema.define(version: 20150501044930) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "topic"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150501041209) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
